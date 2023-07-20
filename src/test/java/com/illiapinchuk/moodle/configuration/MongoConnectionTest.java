33ae0832b4106b938de7e63c77673437abf8ca53
Illia Pinchuk <1gtagat@gmail.com>
2023-07-20 14:16:22 +0300

    [Task 22] Wrote unit tests.

--- src/test/java/com/illiapinchuk/moodle/configuration/MongoConnectionTest.java
+++ src/test/java/com/illiapinchuk/moodle/configuration/MongoConnectionTest.java
@@ -8,12 +8,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 class MongoConnectionTest {
 
   @DisplayName(
---
+++ src/test/java/com/illiapinchuk/moodle/service/impl/EmailServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.illiapinchuk.moodle.service.impl;
+
+import com.illiapinchuk.moodle.common.TestConstants;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class EmailServiceImplTest {
+
+  @InjectMocks private EmailServiceImpl emailService;
+
+  @Mock private JavaMailSender mailSender;
+
+  @Test
+  void sendEmailShouldCallMailSenderSend() {
+    final var emailLetter = new SimpleMailMessage();
+    emailLetter.setFrom("support@mail.com");
+    emailLetter.setTo(TestConstants.EmailConstants.VALID_EMAIL_DTO.getTo());
+    emailLetter.setSubject(TestConstants.EmailConstants.VALID_EMAIL_DTO.getSubject());
+    emailLetter.setText(TestConstants.EmailConstants.VALID_EMAIL_DTO.getContent());
+
+    doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+    emailService.sendEmail(TestConstants.EmailConstants.VALID_EMAIL_DTO);
+
+    verify(mailSender, times(1)).send(any(SimpleMailMessage.class));
+  }
+}
