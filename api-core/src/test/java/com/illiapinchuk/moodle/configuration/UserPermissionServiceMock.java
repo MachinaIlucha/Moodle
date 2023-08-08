46eb823580f45e2b5ca80dc0749a1690c1caf5cc
Illia Pinchuk <1gtagat@gmail.com>
2023-08-08 23:08:29 +0300

    [Task 32] Fixed CourseServiceImplTest tests.

---
+++ api-core/src/test/java/com/illiapinchuk/moodle/configuration/UserPermissionServiceMock.java
@@ -0,0 +1,30 @@
+package com.illiapinchuk.moodle.configuration;
+
+import com.illiapinchuk.moodle.common.TestConstants;
+import com.illiapinchuk.moodle.configuration.security.UserPermissionService;
+import org.mockito.MockedStatic;
+
+import static org.mockito.Mockito.mockStatic;
+
+public class UserPermissionServiceMock {
+    private static MockedStatic<UserPermissionService> mockedUserPermissionService;
+
+    public static void start() {
+        if (mockedUserPermissionService != null) {
+            mockedUserPermissionService.close();
+        }
+        mockedUserPermissionService = mockStatic(UserPermissionService.class);
+        mockedUserPermissionService
+                .when(UserPermissionService::getJwtUser)
+                .thenReturn(TestConstants.UserConstants.ADMIN_JWT_USER);
+        mockedUserPermissionService.when(UserPermissionService::hasAnyRulingRole).thenReturn(true);
+    }
+
+    public static void stop() {
+        if (mockedUserPermissionService != null) {
+            mockedUserPermissionService.close();
+            mockedUserPermissionService = null;
+        }
+    }
+}
+
