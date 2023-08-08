5a0c0918e1d4f9294dbff5da26223905cf3a2a1b
Illia Pinchuk <1gtagat@gmail.com>
2023-08-08 23:14:38 +0300

    [Task 32] Fixed CourseServiceImplTest tests.

--- api-core/src/test/java/com/illiapinchuk/moodle/configuration/UserPermissionServiceMock.java
+++ api-core/src/test/java/com/illiapinchuk/moodle/configuration/UserPermissionServiceMock.java
@@ -7,24 +7,23 @@ import org.mockito.MockedStatic;
 import static org.mockito.Mockito.mockStatic;
 
 public class UserPermissionServiceMock {
-    private static MockedStatic<UserPermissionService> mockedUserPermissionService;
+  private static MockedStatic<UserPermissionService> mockedUserPermissionService;
 
-    public static void start() {
-        if (mockedUserPermissionService != null) {
-            mockedUserPermissionService.close();
-        }
-        mockedUserPermissionService = mockStatic(UserPermissionService.class);
-        mockedUserPermissionService
-                .when(UserPermissionService::getJwtUser)
-                .thenReturn(TestConstants.UserConstants.ADMIN_JWT_USER);
-        mockedUserPermissionService.when(UserPermissionService::hasAnyRulingRole).thenReturn(true);
+  public static synchronized void start() {
+    if (mockedUserPermissionService != null) {
+      mockedUserPermissionService.close();
     }
+    mockedUserPermissionService = mockStatic(UserPermissionService.class);
+    mockedUserPermissionService
+        .when(UserPermissionService::getJwtUser)
+        .thenReturn(TestConstants.UserConstants.ADMIN_JWT_USER);
+    mockedUserPermissionService.when(UserPermissionService::hasAnyRulingRole).thenReturn(true);
+  }
 
-    public static void stop() {
-        if (mockedUserPermissionService != null) {
-            mockedUserPermissionService.close();
-            mockedUserPermissionService = null;
-        }
+  public static synchronized void stop() {
+    if (mockedUserPermissionService != null) {
+      mockedUserPermissionService.close();
+      mockedUserPermissionService = null;
     }
+  }
 }
-
